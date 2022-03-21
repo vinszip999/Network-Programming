93976b24721359aa628e646a15149dfe680eb763
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-21 14:42:07 +0900

    playground/ -> bye/ -> say_bye_html() -> bye.html

--- playground/urls.py
+++ playground/urls.py
@@ -6,4 +6,5 @@ app_name = 'playground'
 urlpatterns = [
     path('hello/', views.say_hello, name='hello'),  # playground:hello
     path('hello_html/', views.say_hello_html, name='hello_html'),  # views.say_hello_html 뒤에 괄호 없어야 하고, 문장 끝날때 꼭 , 넣어주기
+    path('bye_html/', views.say_bye_html, name='bye_html'),
 ]
--- playground/views.py
+++ playground/views.py
@@ -8,3 +8,6 @@ def say_hello(request):
 def say_hello_html(request):
     return render(request, 'playground/hello.html')  # 만들고서 웹페이지를 보이게 할 때 쓰임
 
+
+def say_bye_html(request):
+    return render(request, 'playground/bye.html')
