56e82dc7322ced0afdc88cdd7a2646d3b6909497
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-21 14:24:58 +0900

    playground/urls: hello_html

--- playground/urls.py
+++ playground/urls.py
@@ -5,4 +5,5 @@ from playground import views
 app_name = 'playground'
 urlpatterns = [
     path('hello/', views.say_hello, name='hello'),  # playground:hello
+    path('hello_html/', views.say_hello_html, name='hello_html'),  # views.say_hello_html 뒤에 괄호 없어야 하고, 문장 끝날때 꼭 , 넣어주기
 ]
