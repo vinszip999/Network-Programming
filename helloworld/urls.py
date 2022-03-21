13c8335648e3914cbddfff2b72580bc1cb5ff868
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-21 13:50:11 +0900

    urls, playground/urls: playground/ -> hello/ -> say_hello()

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-import playground.views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('playground/hello/', playground.views.say_hello),  # say_hello 뒤에 *괄호가 있으면 안된다*
+    path('playground/', include('playground.urls')),
     path('admin/', admin.site.urls),
 ]
