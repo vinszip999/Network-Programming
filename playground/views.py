eb574d4b6e11993531336183bb3ef3b58fa04a5d
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-21 14:24:09 +0900

    templates: playground/ -> hello_html/ -> say_hello_html() -> html

--- playground/views.py
+++ playground/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def say_hello(request):
     return HttpResponse("Hello, World!")
+
+
+def say_hello_html(request):
+    return render(request, 'playground/hello.html')  # 만들고서 웹페이지를 보이게 할 때 쓰임
+
